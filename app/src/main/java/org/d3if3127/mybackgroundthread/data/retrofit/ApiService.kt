85636beb7dc92295fea7cd1cc9288a692a372a5d
FadlyRamdhani4503 <91235752+FadlyRamdhani4503@users.noreply.github.com>
2023-09-24 15:12:24 +0700

    nice beres coyyy

---
+++ app/src/main/java/org/d3if3127/mybackgroundthread/data/retrofit/ApiService.kt
@@ -0,0 +1,21 @@
+package org.d3if3127.mybackgroundthread.data.retrofit
+
+import org.d3if3127.mybackgroundthread.data.response.PostReviewResponse
+import org.d3if3127.mybackgroundthread.data.response.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
+}

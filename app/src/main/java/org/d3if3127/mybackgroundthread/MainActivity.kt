e94e010eff651d92d418598c1c3e34ceecaa5950
FadlyRamdhani4503 <91235752+FadlyRamdhani4503@users.noreply.github.com>
2023-09-24 19:22:30 +0700

    nice selesai

--- app/src/main/java/org/d3if3127/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/org/d3if3127/mybackgroundthread/MainActivity.kt
@@ -2,10 +2,49 @@ package org.d3if3127.mybackgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import org.d3if3127.mybackgroundthread.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        displayResults()
+
+        activityMainBinding.btnCalculate.setOnClickListener {
+
+            val width = activityMainBinding.edtWidth.text.toString()
+            val height = activityMainBinding.edtHeight.text.toString()
+            val length = activityMainBinding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() -> {
+                    activityMainBinding.edtWidth.error = "Masih kosong"
+                }
+                height.isEmpty() -> {
+                    activityMainBinding.edtHeight.error = "Masih kosong"
+                }
+                length.isEmpty() -> {
+                    activityMainBinding.edtLength.error = "Masih kosong"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResults()
+                }
+            }
+        }
+    }
+
+    private fun displayResults() {
+        activityMainBinding.tvResult.text = viewModel.result.toString()
     }
 }
